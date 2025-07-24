3765c13b6a87047e48d1fe701780406c26cb799d
Sandro <alexanderkapanadze52@gmail.com>
2025-07-24 17:37:43 +0200

    finished implementing feedback page

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,7 @@
+import type { ClassValue } from "clsx"
+import clsx from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function formatSize(bytes: number): string {
   if (bytes < 1024) return `${bytes} B`
   const kb = bytes / 1024
@@ -9,4 +13,8 @@ export function formatSize(bytes: number): string {
 }
 export function generateUUID() {
   return crypto.randomUUID()
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
